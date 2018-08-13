d0280513d524952349212531d1663137d807c984
Ingmar Delsink <ingmardelsink@hotmail.com>
2018-08-13 23:58:56 +0200

    Show tracks as cards in a grid

---
+++ src/components/track-card.vue
@@ -0,0 +1,66 @@
+<template lang="html">
+  <v-card tile class="white--text">
+    <v-layout>
+      <v-flex xs4>
+        <v-layout align-center justify-center fill-height>
+          <v-flex>
+            <v-card-media
+            :src="albumImage"
+            height="125px"
+            contain
+            ></v-card-media>
+          </v-flex>
+        </v-layout>
+        <!-- <v-card-media
+          :src="albumImage"
+          height="125px"
+          contain
+        ></v-card-media> -->
+      </v-flex>
+      <v-flex xs8>
+        <v-card-title primary-title>
+          <div>
+            <div class="headline">{{trackName}}</div>
+            <div>{{artistNames}}</div>
+            <div>{{albumName}}</div>
+          </div>
+        </v-card-title>
+      </v-flex>
+    </v-layout>
+    <v-divider light></v-divider>
+  </v-card>
+</template>
+
+<script>
+import _ from 'lodash';
+
+export default {
+  name: 'TrackCard',
+  props: {
+    data: {
+      type: Object,
+      default: {}
+    }
+  },
+  data: function () {
+    return {};
+  },
+  computed: {
+    trackName: function () {
+      return _.get(this.data, 'name', '');
+    },
+    artistNames: function () {
+      return _.map(_.get(this.data, 'artists', []), artist => _.get(artist, 'name', '')).join(', ');
+    },
+    albumName: function () {
+      return _.get(this.data, 'album.name', '');
+    },
+    albumImage: function () {
+      return _.get(_.first(_.get(this.data, 'album.images', [])), 'url', '');
+    }
+  }
+};
+</script>
+
+<style lang="css" scoped>
+</style>
--- src/views/Player.vue
+++ src/views/Player.vue
@@ -1,52 +1,36 @@
 <template lang="html">
   <div>
     <v-toolbar app fixed clipped-left><v-toolbar-title>Spotify track preview</v-toolbar-title></v-toolbar>
-      <v-container fluid>
-        <v-content v-if="apiAvailable">
-          <v-layout align-center justify-center>
-            <v-flex xs12 sm6>
-              <v-text-field
-                :loading="isSearching"
-                clearable
-                label="Search Spotify"
-                hint="For example, Tracks or Albums"
-                v-model="searchQuerry"
-              ></v-text-field>
+    <v-container :grid-list-xs="true" fluid>
+      <v-content v-if="apiAvailable">
+        <v-layout align-center justify-center>
+          <v-flex xs12 sm6>
+            <v-text-field
+              :loading="isSearching"
+              clearable
+              label="Search Spotify"
+              hint="For example, Tracks or Albums"
+              v-model="searchQuerry"
+            ></v-text-field>
+          </v-flex>
+        </v-layout>
+        <v-container :grid-list-xs="true" fill-height fluid>
+          <v-layout row wrap>
+            <v-flex
+              v-for="item in searchResults"
+              xs12 sm6 md4 lg4 xl3
+            >
+              <trackCard
+                :data="item"
+              ></trackCard>
             </v-flex>
           </v-layout>
-          <v-layout>
-            <v-flex xs12>
-              <template v-for="item in searchResults">
-                <v-card class="white--text">
-                  <v-layout>
-                    <v-flex xs5>
-                      <v-card-media
-                        :src="item.album.images[0].url"
-                        height="125px"
-                        contain
-                      ></v-card-media>
-                    </v-flex>
-                    <v-flex xs7>
-                      <v-card-title primary-title>
-                        <div>
-                          <div class="headline">{{item.name}}</div>
-                          <!-- This is bad and I should feel bad, I will change this later -->
-                          <div>{{item.artists[0].name}}</div>
-                          <div>{{item.album.name}}</div>
-                        </div>
-                      </v-card-title>
-                    </v-flex>
-                  </v-layout>
-                  <v-divider light></v-divider>
-                </v-card>
-              </template>
-            </v-flex>
-        </v-layout>
+        </v-container>
       </v-content>
       <v-content v-else>
         <v-container fluid>
           <v-layout column align-center justify-center row fill-height>
-            <v-flex xs12>
+            <v-flex xs12 sm6 offset-sm3>
               <v-progress-circular
                 :size="70"
                 color="primary"
@@ -64,10 +48,12 @@
 import _ from 'lodash';
 import Promise from 'bluebird';
 import SpotifyWebApi from 'spotify-web-api-js';
+import trackCard from '../components/track-card';
 
 export default {
   name: 'Player',
   components: {
+    trackCard
   },
   data: () => {
     const Spotify = new SpotifyWebApi();
@@ -123,6 +109,10 @@ export default {
       console.log('result from getMe', data);
     }).catch(err => {
       console.error('Error while calling the API', err);
+      if (_.get(err, 'status') === 401) {
+        console.warn('Unauthorized');
+        this.getYourselfAuthenticated();
+      }
     });
   }
 };
